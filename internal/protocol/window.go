9ff78777a9d38ef9168e92404ae9b14a6325d44e
mmadfox <sergey.liskonog@gmail.com>
2020-02-26 05:23:53 +0300

    added window api & some refactor

--- internal/protocol/window.go
+++ internal/protocol/window.go
@@ -1,6 +1,9 @@
 package protocol
 
-import "context"
+import (
+	"context"
+	"encoding/json"
+)
 
 const (
 	// Maximized the window is maximized.
@@ -18,9 +21,11 @@ const (
 
 type WindowState string
 
-type Size struct {
-	Width  int
-	Height int
+type Rect struct {
+	Width  int  `json:"width"`
+	Height int  `json:"height"`
+	Y      uint `json:"y"`
+	X      uint `json:"x"`
 }
 
 type Window struct {
@@ -37,6 +42,57 @@ func (w Window) SessionID() string {
 	return w.sessionID
 }
 
-func (w Window) SetSize(ctx context.Context, s Size) error {
+// SetRect change the size of the specified window.
+// https://www.w3.org/TR/webdriver1/#set-window-rect
+func (w Window) SetRect(ctx context.Context, r Rect) error {
+	params := params{
+		"width":  r.Width,
+		"height": r.Height,
+		"x":      r.X,
+		"y":      r.Y,
+	}
+	if _, err := w.client.Post(ctx, "/session/"+w.sessionID+"/window/rect", params); err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetRect get the size of the specified window.
+// https://www.w3.org/TR/webdriver1/#get-window-rect
+func (w Window) GetRect(ctx context.Context) (r Rect, err error) {
+	resp, err := w.client.Get(ctx, "/session/"+w.sessionID+"/window/rect")
+	if err != nil {
+		return r, err
+	}
+	if err := json.Unmarshal(resp.Value, &r); err != nil {
+		return r, err
+	}
+	return r, nil
+}
+
+// Maximize the specified window if not already maximized.
+// https://www.w3.org/TR/webdriver1/#maximize-window
+func (w Window) Maximize(ctx context.Context) error {
+	if _, err := w.client.Post(ctx, "/session/"+w.sessionID+"/window/maximize", nil); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Minimize the specified window if not already minimized.
+// https://www.w3.org/TR/webdriver1/#minimize-window
+func (w Window) Minimize(ctx context.Context) error {
+	if _, err := w.client.Post(ctx, "/session/"+w.sessionID+"/window/minimize", nil); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Fullscreen the specified window.
+// https://www.w3.org/TR/webdriver1/#fullscreen-window
+func (w Window) Fullscreen(ctx context.Context) error {
+	if _, err := w.client.Post(ctx, "/session/"+w.sessionID+"/window/fullscreen ", nil); err != nil {
+		return err
+	}
 	return nil
 }
--- session.go
+++ session.go
@@ -13,20 +13,20 @@ const (
 	DefaultTimeout    = 15 * time.Second
 )
 
-type Browser struct {
+type Session struct {
 	session    *protocol.Session
 	navigation *protocol.Navigation
 	commandCtx *protocol.CommandContext
 }
 
-func NewBrowserFromClient(client httpclient.Client, desired, required protocol.Capabilities) (*Browser, error) {
+func NewSessionFromClient(client httpclient.Client, desired, required protocol.Capabilities) (*Session, error) {
 	cli := protocol.NewClient(client)
 	sess, err := protocol.NewSession(cli, desired, required)
 	if err != nil {
 		return nil, err
 	}
 
-	browser := Browser{
+	browser := Session{
 		session:    sess,
 		navigation: protocol.NewNavigation(cli, sess.ID()),
 		commandCtx: protocol.NewCommandContext(cli, sess.ID()),
@@ -34,7 +34,7 @@ func NewBrowserFromClient(client httpclient.Client, desired, required protocol.C
 	return &browser, nil
 }
 
-func NewBrowser(addr string, desired, required protocol.Capabilities) (*Browser, error) {
+func NewSession(addr string, desired, required protocol.Capabilities) (*Session, error) {
 	client, err := httpclient.NewClient(addr,
 		httpclient.WithRetryCount(DefaultRetryCount),
 		httpclient.WithTimeout(DefaultTimeout),
@@ -42,21 +42,21 @@ func NewBrowser(addr string, desired, required protocol.Capabilities) (*Browser,
 	if err != nil {
 		return nil, err
 	}
-	return NewBrowserFromClient(client, desired, required)
+	return NewSessionFromClient(client, desired, required)
 }
 
-func (b *Browser) Session() *protocol.Session {
+func (b *Session) Session() *protocol.Session {
 	return b.session
 }
 
-func (b *Browser) Navigation() *protocol.Navigation {
+func (b *Session) Navigation() *protocol.Navigation {
 	return b.navigation
 }
 
-func (b *Browser) CommandContext() *protocol.CommandContext {
+func (b *Session) CommandContext() *protocol.CommandContext {
 	return b.commandCtx
 }
 
-func (b *Browser) Close(ctx context.Context) error {
+func (b *Session) Close(ctx context.Context) error {
 	return b.session.Delete(ctx)
 }
