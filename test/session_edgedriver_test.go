79156510ce9361ecef3567ca7bfe6b32e96cbcce
mmadfox <sergey.liskonog@gmail.com>
2020-02-26 07:55:23 +0300

    added firefoxdriver service for int tests

--- test/session_edgedriver_test.go
+++
@@ -1,7 +0,0 @@
-package test
-
-import "testing"
-
-func TestSessionEdgesdriver(t *testing.T) {
-	t.Log("session_edgedriver_test")
-}
---
+++ test/session_firefoxdriver_test.go
@@ -0,0 +1,7 @@
+package test
+
+import "testing"
+
+func TestSessionFirefoxdriver(t *testing.T) {
+	t.Log("session_firefoxdriver_test")
+}
